6fbfeecc5ba32d64f8e81a0712b57217b3cca53f
Augusto Gonzalez <augusto@MacBook-Pro-de-Augusto.local>
2018-11-08 11:51:02 -0500

    React Strap basic structure and tab include

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,25 +1,99 @@
 import React, { Component } from 'react'
-import logo from '../Assets/logo.svg'
 import './Styles/App.css'
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap'
+import classnames from 'classnames'
 
 class App extends Component {
+  state = {
+    activeTab: '2'
+  }
+
+  toggle = (tab) => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      })
+    }
+  }
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Nav tabs className='row justify-content-center'>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.toggle('1') }}
+            >
+          HOME
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => { this.toggle('2') }}
+            >
+          PRODUCTS
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3' })}
+              onClick={() => { this.toggle('3') }}
+            >
+          CLIENTS
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '4' })}
+              onClick={() => { this.toggle('4') }}
+            >
+          CONTACT
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId='1'>
+            <Row className='justify-content-center'>
+              <Col sm='12' md='12' className='justify-content-center'>
+                <h1>Welcome to Team Business!</h1>
+                <h3>We got the best products for you!</h3>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId='2'>
+            <Row>
+              <Col sm='6'>
+                <Card body>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+              <Col sm='6'>
+                <Card body>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId='3'>
+            <Row>
+              <Col sm='12'>
+                <h4>Clients everywhere!</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId='4'>
+            <Row>
+              <Col sm='12'>
+                <h4>Tab 4 Contents</h4>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     )
   }
